120e225af3602aaf85edd4df030bca55c8d78389
paiva737 <rafaelpaiva636@gmail.com>
2025-08-11 20:46:50 -0300

    auto-preencher endereço via ViaCEP no blur do CEP

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(function () {
-  console.log("App iniciado")
+  function onlyDigits(s){return s.replace(/\D/g,'')}
+  function formatCEP(d){return d.replace(/(\d{5})(\d{3})/,'$1-$2')}
+
+  $('#cep').on('input',function(){
+    this.value = onlyDigits(this.value).slice(0,8)
+  })
+
+  $('#cep').on('blur',function(){
+    const cep = onlyDigits(this.value)
+    if(cep.length!==8) return
+    $.getJSON('https://viacep.com.br/ws/'+cep+'/json/',function(d){
+      if(d && !d.erro){
+        $('#cep').val(formatCEP(cep))
+        $('#logradouro').val(d.logradouro||'')
+        $('#bairro').val(d.bairro||'')
+        $('#municipio').val(d.localidade||'')
+        $('#uf').val(d.uf||'')
+        $('#complemento').val(d.complemento||'')
+      }
+    })
+  })
 })
